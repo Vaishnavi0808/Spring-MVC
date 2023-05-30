d71c23d7e6694bac31549f8a84d6c131098a9137
Vaishnavi Sarode <sarodevaishnavi3@gmail.com>
2023-05-30 23:12:26 +0530

    Add files via upload

---
+++ BootToMVC/src/test/java/com/boottomvc/BootToMVC/BootToMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boottomvc.BootToMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootToMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
