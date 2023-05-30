d70baa6708b893905a316a84a5a26baf0dcbaf7c
Vaishnavi Sarode <sarodevaishnavi3@gmail.com>
2023-05-30 22:48:26 +0530

    Add files via upload

---
+++ Mvc1/src/main/java/com/vaishnavi/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.vaishnavi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "hello";
+	}
+
+}
